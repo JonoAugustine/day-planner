fd1fb045eb83b6fc0eb606782f8bf08483e278c8
JonoAugustine <jaugust8@jhu.edu>
2019-11-06 00:47:14 -0500

    work on new framework. need to move static styling to css files since JQry wont overwrite

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,3 +1,73 @@
-const TimeFrame = () => {
-    
-}
+const TodoArea = state => {
+  return elOf(
+    "input",
+    {
+      width: "100%",
+      height: "100%",
+      float: "left",
+      "background-color": "green"
+    },
+    input => {
+      input.attr("value", state ? state.text : "");
+      input.attr("type", "textArea");
+    }
+  );
+};
+
+const SaveButton = (state, todo) => {
+  return elOf(
+    "div",
+    {
+      width: "10%",
+      height: "100%",
+      float: "left",
+      "background-color": "aqua"
+    },
+    div => {
+      // TODO save functionality
+    }
+  );
+};
+
+const TimeFrame = (time, state) => {
+  const todo = TodoArea(state);
+  const save = SaveButton(state, todo);
+  return elOf(
+    "div",
+    {
+      height: "50px",
+      width: "100%",
+      display: "flex",
+      "border-top": "1px solid"
+    },
+    null,
+    elOf(
+      "div",
+      {
+        width: "10%",
+        float: "left",
+        "text-align": "right"
+      },
+      div => {
+        div.text("time");
+      }
+    ),
+    todo,
+    save
+  );
+};
+
+const TimeFrameSection = () => {
+  return elOf(
+    "div",
+    {
+      height: "100%",
+      width: "70%",
+      margin: "0 auto"
+    },
+    () => {},
+    TimeFrame()
+  );
+};
+
+show(TimeFrameSection);
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,35 @@
-const root = document.getElementById("root");
+const root = $("#root");
 
 /**
- * Generates a style string from an object.
- * @param {*} styleSpec
+ * Creates a JQuery element and applies the given style.
+ * @param {string} tag
+ * @param {*} style
+ * @param {function} callback
+ * @returns the created element.
  */
-const styleOf = styleSpec =>
-  styleSpec.entries.map((k, v) => `${k}:${v};`).join();
-
-const elementOf = (tag, style, callback) => {
-  const e = document.createElement(tag);
-  e.setAttribute("style", styleOf(style));
+const elOf = (tag, style, callback, ...children) => {
+  const e = $(`<${tag}>`).css(style);
+  if (callback) callback(e);
+  if (children) children.forEach(c => e.append(c));
+  return e;
 };
+
+/**
+ * Clear's the given component ot the root if null.
+ * @param {*} component
+ */
+const clear = component => (component ? component : root).empty();
+
+/**
+ * Clears root and display's component
+ * @param {function} component
+ */
+const show = component => {
+  root.empty();
+  root.append(component());
+};
+
+const TimeFrameState = time => ({
+  time: time,
+  saved: false
+});
